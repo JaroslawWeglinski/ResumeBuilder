8d47c32e6f8ed7da705ea66c1d6dabe06eac523a
Jarek <j.weglinski2@gmail.com>
2017-05-29 19:03:14 +0200

    init

---
+++ src/main/java/pl/novaris/resumebuilder/ServletInitializer.java
@@ -0,0 +1,13 @@
+package pl.novaris.resumebuilder;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ResumeBuilderApplication.class);
+	}
+
+}
