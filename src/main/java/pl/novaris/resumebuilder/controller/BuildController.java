4858e21ba724b8f4f5d6b06bd503889c088ae54b
Jarek <j.weglinski2@gmail.com>
2017-06-01 01:10:09 +0200

    modified endpoints

--- src/main/java/pl/novaris/resumebuilder/controller/BuildController.java
+++ src/main/java/pl/novaris/resumebuilder/controller/BuildController.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import pl.novaris.resumebuilder.util.PdfGeneratorUtil;
 
@@ -18,30 +19,28 @@ public class BuildController {
     @Autowired
     PdfGeneratorUtil pdfGeneratorUtil;
 
-    static private Map<String,String> data = new HashMap<>();
+    static private Map<String,String> buildData = new HashMap<>();
 
-    @RequestMapping("/personalData")
+    @RequestMapping(value = "/personalData", method = RequestMethod.POST)
     public void addPersonalData(Optional<String> maybePersonalData) throws Exception {
         String personalData = maybePersonalData.filter(StringUtils::isNotBlank).orElse("unknown");
 
-        Map<String,String> data = new HashMap<>();
-        data.put("personalData",personalData);
+        buildData.put("personalData",personalData);
 
     }
 
-    @RequestMapping("/education")
+    @RequestMapping(value = "/education", method = RequestMethod.POST)
     public void addEducation(Optional<String> maybeEducation) throws Exception {
         String education = maybeEducation.filter(StringUtils::isNotBlank).orElse("unknown");
 
-        Map<String,String> data = new HashMap<>();
-        data.put("education",education);
+        buildData.put("education",education);
 
     }
 
     @RequestMapping("/generate")
     public void generatePdf() {
         try {
-            pdfGeneratorUtil.createPdf("test", data);
+            pdfGeneratorUtil.createPdf("test", buildData);
         } catch (Exception e) {
             e.printStackTrace();
         }
