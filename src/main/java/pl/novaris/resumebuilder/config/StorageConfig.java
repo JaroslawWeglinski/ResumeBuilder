8e816257544c109da3da56d5303f572101b2c68b
Jarek <j.weglinski2@gmail.com>
2017-08-24 19:26:46 +0200

    image upload - changed target filename

--- src/main/java/pl/novaris/resumebuilder/config/StorageConfig.java
+++ src/main/java/pl/novaris/resumebuilder/config/StorageConfig.java
@@ -2,13 +2,20 @@ package pl.novaris.resumebuilder.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.ResourceBundle;
+
 @ConfigurationProperties("storage")
 public class StorageConfig {
 
     /**
      * Folder location for storing files
      */
+
+    private ResourceBundle resourceBundle = ResourceBundle.getBundle("configuration");
+
     private String location = "upload-dir";
+
+    private String targetFilename = resourceBundle.getString("target.filename");
 
     public String getLocation() {
         return location;
@@ -18,4 +25,11 @@ public class StorageConfig {
         this.location = location;
     }
 
+    public String getTargetFilename() {
+        return targetFilename;
+    }
+
+    public void setTargetFilename(String targetFilename) {
+        this.targetFilename = targetFilename;
+    }
 }
--- src/main/java/pl/novaris/resumebuilder/service/impl/FileSystemStorageServiceImpl.java
+++ src/main/java/pl/novaris/resumebuilder/service/impl/FileSystemStorageServiceImpl.java
@@ -20,86 +20,89 @@ import org.springframework.util.FileSystemUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-    @Service
-    public class FileSystemStorageServiceImpl implements StorageService {
+@Service
+public class FileSystemStorageServiceImpl implements StorageService {
 
-        private final Path rootLocation;
+    private final Path rootLocation;
+    private final String targetFilename;
 
-        @Autowired
-        public FileSystemStorageServiceImpl(StorageConfig properties) {
-            this.rootLocation = Paths.get(properties.getLocation());
+    @Autowired
+    public FileSystemStorageServiceImpl(StorageConfig properties) {
+        this.rootLocation = Paths.get(properties.getLocation());
+        this.targetFilename = properties.getTargetFilename();
+    }
+
+    @Override
+    public void store(MultipartFile file) {
+        String filename;
+        if (targetFilename == null) {
+            filename = StringUtils.cleanPath(file.getOriginalFilename());
+        } else {
+            filename = targetFilename;
         }
-
-        @Override
-        public void store(MultipartFile file) {
-            String filename = StringUtils.cleanPath(file.getOriginalFilename());
-            try {
-                if (file.isEmpty()) {
-                    throw new StorageException("Failed to store empty file " + filename);
-                }
-                if (filename.contains("..")) {
-                    // This is a security check
-                    throw new StorageException(
-                            "Cannot store file with relative path outside current directory "
-                                    + filename);
-                }
-                Files.copy(file.getInputStream(), this.rootLocation.resolve(filename),
-                        StandardCopyOption.REPLACE_EXISTING);
+        try {
+            if (file.isEmpty()) {
+                throw new StorageException("Failed to store empty file " + filename);
             }
-            catch (IOException e) {
-                throw new StorageException("Failed to store file " + filename, e);
+            if (filename.contains("..")) {
+                // This is a security check
+                throw new StorageException(
+                        "Cannot store file with relative path outside current directory "
+                                + filename);
             }
-        }
-
-        @Override
-        public Stream<Path> loadAll() {
-            try {
-                return Files.walk(this.rootLocation, 1)
-                        .filter(path -> !path.equals(this.rootLocation))
-                        .map(path -> this.rootLocation.relativize(path));
-            }
-            catch (IOException e) {
-                throw new StorageException("Failed to read stored files", e);
-            }
-
-        }
-
-        @Override
-        public Path load(String filename) {
-            return rootLocation.resolve(filename);
-        }
-
-        @Override
-        public Resource loadAsResource(String filename) {
-            try {
-                Path file = load(filename);
-                Resource resource = new UrlResource(file.toUri());
-                if (resource.exists() || resource.isReadable()) {
-                    return resource;
-                }
-                else {
-                    throw new StorageFileNotFoundException(
-                            "Could not read file: " + filename);
-
-                }
-            }
-            catch (MalformedURLException e) {
-                throw new StorageFileNotFoundException("Could not read file: " + filename, e);
-            }
-        }
-
-        @Override
-        public void deleteAll() {
-            FileSystemUtils.deleteRecursively(rootLocation.toFile());
-        }
-
-        @Override
-        public void init() {
-            try {
-                Files.createDirectories(rootLocation);
-            }
-            catch (IOException e) {
-                throw new StorageException("Could not initialize storage", e);
-            }
+            Files.copy(file.getInputStream(), this.rootLocation.resolve(filename),
+                    StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("File successfully stored!");
+        } catch (IOException e) {
+            throw new StorageException("Failed to store file " + filename, e);
         }
     }
+
+    @Override
+    public Stream<Path> loadAll() {
+        try {
+            return Files.walk(this.rootLocation, 1)
+                    .filter(path -> !path.equals(this.rootLocation))
+                    .map(path -> this.rootLocation.relativize(path));
+        } catch (IOException e) {
+            throw new StorageException("Failed to read stored files", e);
+        }
+
+    }
+
+    @Override
+    public Path load(String filename) {
+        return rootLocation.resolve(filename);
+    }
+
+    @Override
+    public Resource loadAsResource(String filename) {
+        try {
+            Path file = load(filename);
+            Resource resource = new UrlResource(file.toUri());
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new StorageFileNotFoundException(
+                        "Could not read file: " + filename);
+
+            }
+        } catch (MalformedURLException e) {
+            throw new StorageFileNotFoundException("Could not read file: " + filename, e);
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        FileSystemUtils.deleteRecursively(rootLocation.toFile());
+    }
+
+    @Override
+    public void init() {
+        try {
+            Files.createDirectories(rootLocation);
+        } catch (IOException e) {
+            throw new StorageException("Could not initialize storage", e);
+        }
+    }
+}
